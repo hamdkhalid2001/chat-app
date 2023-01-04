c79b61b6d010622aaaedc885ec05ac7c09a2784f
Hamd Khalid <hamdbinkhalid@gmail.com>
2023-01-04 22:29:30 +0500

    Added tailwind css

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App w-full max-w-[900px] self-center flex flex-col justify-center">
+      <h1 className='text-3xl text-red-500'>Hello World</h1>
     </div>
   );
 }
