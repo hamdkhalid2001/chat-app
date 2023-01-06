9967f502ee0329735df770f83db848e529f30b4a
Hamd Khalid <hamdbinkhalid@gmail.com>
2023-01-06 11:51:19 +0500

    Connect socket server with client

--- client/src/components/ChatPage/SendMessage.js
+++ client/src/components/ChatPage/SendMessage.js
@@ -1,18 +1,20 @@
 import React from "react";
-
+import { SocketContext, socket } from "../../contexts/Socket";
 function SendMessage() {
   return (
     <section className=" w-full">
-      <div>
-        <div className="bg-gray-200 flex pr-3 justify-between">
-          <input type="text" className="w-[70%]" placeholder="Send Message" />
-          <img
-            src={require("../../assets/images/send-icon.png")}
-            alt=""
-            className="self-center"
-          />
+      <SocketContext.Provider value={socket}>
+        <div>
+          <div className="bg-gray-200 flex pr-3 justify-between">
+            <input type="text" className="w-[70%]" placeholder="Send Message" />
+            <img
+              src={require("../../assets/images/send-icon.png")}
+              alt=""
+              className="self-center"
+            />
+          </div>
         </div>
-      </div>
+      </SocketContext.Provider>
     </section>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
-const express = require("express");
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
+const httpServer = require("http").createServer();
+const cors = require("cors");
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
 });
 
-server.listen(4000, () => {
-  console.log("listening on *:4000");
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+httpServer.listen(4000, () => {
+  console.log("Server running on 4000");
 });
